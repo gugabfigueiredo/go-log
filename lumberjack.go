0b22348f6c7aa5cac3327b784673cdd30f77e2b0
Guga Figueiredo <gugabfigueiredo@gmail.com>
2024-03-02 07:53:41 +0100

    move lumberjack setup to its own file

---
+++ lumberjack.go
@@ -0,0 +1,23 @@
+package log
+
+import (
+	"github.com/rs/zerolog/log"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"io"
+	"os"
+	"path"
+)
+
+func newRollingFile(config *Config) io.Writer {
+	if err := os.MkdirAll(config.Directory, 0744); err != nil {
+		log.Error().Err(err).Str("path", config.Directory).Msg("can't create log directory")
+		return nil
+	}
+
+	return &lumberjack.Logger{
+		Filename:   path.Join(config.Directory, config.Context, config.Filename),
+		MaxBackups: config.MaxBackups, // files
+		MaxSize:    config.MaxSize,    // megabytes
+		MaxAge:     config.MaxAge,     // days
+	}
+}
